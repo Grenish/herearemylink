8e990e276a449a1829c87a6b33c0909682eee958
Grenish rai <ariestheracer@gmail.com>
2024-08-26 19:44:47 +0530

    refactor: Update login button link in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
       <h2>Hello WOrld</h2>
-      <Link href="/dashboard">
-        <button className="p-2 px-5 rounded-lg bg-green-500">Login</button>
+      <Link href="/onboarding/login">
+        <button className="p-2 px-5 rounded-lg bg-green-500">Login here</button>
       </Link>
     </>
   );
