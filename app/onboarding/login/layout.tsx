a60467a94e7e2d24f8cece656f488f3ec22afe22
Grenish rai <ariestheracer@gmail.com>
2024-08-26 19:43:22 +0530

    feat: Add login layout component

---
+++ app/onboarding/login/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "@/app/globals.css";
+
+const mont = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login Form",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={mont.className}>{children}</body>
+    </html>
+  );
+}
